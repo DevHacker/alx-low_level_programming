c879ecaa0f34c27aaac5a4360ded6140e2d4d4d3
Fidele Donvide <fidele_donvide@outlook.com>
2022-03-10 17:56:58 -0800

    4-puts.c file added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include<stdio.h>
+
+/* get into the main method*/
+
+int main(void)
+{
+  /*Declaration an initializing of the variable*/
+  string message = "Programming is like building a multilangual puzzle";
+
+  puts(message);
+
+  return 0;
+
+}
