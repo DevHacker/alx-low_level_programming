1d70c440d3e08574252eb64970f1d54b933ec143
Fidele Donvide <fidele_donvide@outlook.com>
2022-03-10 18:06:40 -0800

    4-puts file added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
 
-/* get into the main method*/
-
 int main(void)
 {
-  /*Declaration an initializing of the variable*/
   char message[] = "Programming is like building a multilangual puzzle";
 
   puts(message);
