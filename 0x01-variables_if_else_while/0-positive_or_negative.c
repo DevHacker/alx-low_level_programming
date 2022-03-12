c0668c5fee776b8e32889e3aad1aa43efa58da1c
Fidele Donvide <fidele_donvide@outlook.com>
2022-03-11 19:05:18 -0800

    0-positive_or_negative file added

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/*
- * Main method entry point of the program
+/* 
+ * Main method
+ *
+ * DESCRIPTION: Entry point of the program
+ *
  */
 int main(void)
 {
@@ -15,18 +18,18 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-	     printf("%d: is positive\n", n);
+	 printf("%d is positive\n", n);
 
 	}
 	else if (n == 0)
 	{
-	     printf("%d: is zero\n", n);
+	 printf("%d is zero\n", n);
 
         }
 	else
 	{
 
-	     printf("%d: is negative\n", n);
+	 printf("%d is negative\n", n);
 
 	}
 	return (0);
