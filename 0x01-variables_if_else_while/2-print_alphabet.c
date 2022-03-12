4681339063423803d93eb745e1ac8c26a4faa8c5
Fidele Donvide <fidele_donvide@outlook.com>
2022-03-11 20:05:35 -0800

    2-print_alphabet file added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * Main method is the entry point of the program
+ *
+ */
+int main(void)
+{
+	char car;
+	/*this for loop to print alphabet*/
+	for (car = 'a'; car <= 'z'; car++)
+	{
+		putchar(car);
+	}
+	return 0;
+
+}
