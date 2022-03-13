8c1ce74f11b8b3f3b7d39e5cda5826237e263b96
DevHacker <99559287+DevHacker@users.noreply.github.com>
2022-03-13 10:55:28 +0000

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  * main -Entry point
- * Description: program that print all single digit numbers of base 10 only with the putchar function
+ * Description: program that print all single digit numbers of base 10
  * Return: Always 0 (Success)
  */
 int main(void)
