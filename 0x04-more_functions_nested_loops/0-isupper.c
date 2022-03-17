ee914d53145eef5530bb70c6801184adce8696cc
Fidele Donvide <fidele_donvide@outlook.com>
2022-03-17 04:40:47 -0700

    Adding of 0x04 projects and its first files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - check if character is on uppercase
+ * Return: return 1 if true in other case return 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# 0x04-more_functions_nested_loops
+
+This Project also contains all works about nested loops in C language, and it's purpose is to give me more understanding about how they works.
