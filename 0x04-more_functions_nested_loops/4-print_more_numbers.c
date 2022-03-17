449491582113006617d9a44b4374be3f09531550
Fidele Donvide <fidele_donvide@outlook.com>
2022-03-17 05:32:33 -0700

    print more numbers functions added

---
+++ 0x04-more_functions_nested_loops/4-print_more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 - 9 except 2, 3
+ * Description: prints
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int numb = 0;
+
+	while (numb <= 9)
+	{
+		if (numb == 2 || a == 4)
+		{
+			continue;
+		}
+		num++;
+	}
+	_putchar('\n');
+}
