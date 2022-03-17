ccb549eb49463e36fa99e89e60c5f9edea602b80
Fidele Donvide <fidele_donvide@outlook.com>
2022-03-17 05:05:30 -0700

    isdigit file added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * _isdigit - check if value is a digit
+ * Return: return 1 if c is a digit
+ *
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	return (0);
+}
