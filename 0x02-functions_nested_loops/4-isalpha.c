4b6f004333885cb029ad713950f45a34d09c1208
Fidele DONVIDE <fidele_donvide@outlook.com>
2022-09-13 19:16:06 +0000

    Check if it's a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+/**
+ * _isalpha - Entry point
+ * Description: Checks if c is a letter
+ * @c: refers to the function's parameter
+ * Return: 1 if letter or 0
+ */
+int _isalpha(int c)
+{
+	int result;
+	if (c >= 'a' && c <= 'z')
+	{
+		result = 1;
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		result = 1;
+	}
+	else
+	{
+		return 0;
+	}
+	return result;
+}
