9360eba3cf1ed94ec1ba61703ce4ca16f1c41b49
DevHacker <99559287+DevHacker@users.noreply.github.com>
2022-09-13 21:14:15 +0000

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,10 +4,12 @@
  * print_sign - Entry point
  * Description: print sign of n
  * @n: refer to funciton's parameter
+ * Return: Always 0 if success
  */
 int print_sign(int n)
 {
 	int result;
+	
 	while (n)
 	{
 		if (n > 0)
@@ -26,7 +28,7 @@ int print_sign(int n)
 		{
 			_putchar(n);
 			result = 0;
-			_putchar(0);
+			break;
 		}
 	}
 	return result;
